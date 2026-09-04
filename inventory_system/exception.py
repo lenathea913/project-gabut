f61906cea01df1fb762df4cab22160ba512be135
lenathea913 <lenathea913@gmail.com>
2026-09-04 23:50:59 +0700

    Add input Validation

--- inventory_system/exception.py
+++ inventory_system/exception.py
@@ -2,3 +2,4 @@ class ProductNotAvailableError(Exception):
     def __init__(self, message='Stok produk habis'):
         self.message = message
         super().__init__(self.message)
+    
--- inventory_system/system.py
+++ inventory_system/system.py
@@ -1,5 +1,6 @@
 from exception import ProductNotAvailableError
-
+import json
+import os
 class Product:
     def __init__(self, nama, harga, stok):
         self.nama = nama
@@ -9,10 +10,35 @@ class Product:
 class Inventory:
     def __init__(self):
         self.products = []
+        self.gudang = os.path.join(os.path.dirname(__file__), 'gudang.json')
+        self.load_products()
+    
+    def load_products(self):
+        self.products.clear()
+        if os.path.exists(self.gudang):
+            try:
+                with open(self.gudang, 'r') as file:
+                    produk_data = json.load(file)
+                    for data in produk_data:
+                        produk = Product(
+                            nama=data['nama'],
+                            harga=data['harga'],
+                            stok=data['stok']
+                        )
+                        self.products.append(produk)
+            except json.JSONDecodeError:
+                print("Error: Gagal memuat data produk. Format JSON tidak valid.")
+        else:
+            print('File gudang.json tidak ditemukan.')
+    
+    def save_data(self):
+        with open(self.gudang, 'w') as file:
+            json.dump([vars(produk) for produk in self.products], file)
     
     def tambah_produk(self, nama, harga, stok):
         produk_baru = Product(nama, harga, stok)
         self.products.append(produk_baru)
+        self.save_data()
         print(f'Produk {nama} berhasil ditambahkan ke inventaris.')
     
     def kurangi_stok(self, nama, jumlah):
@@ -20,6 +46,7 @@ class Inventory:
             if produk.nama == nama:
                 if produk.stok >= jumlah:
                     produk.stok -= jumlah
+                    self.save_data()
                     print(f'Stok produk {nama} berhasil dikurangi sebanyak {jumlah}.')
                 else:
                     raise ProductNotAvailableError(f'Stok produk {nama} tidak mencukupi.')
@@ -31,22 +58,63 @@ class Inventory:
             if produk.nama == nama:
                 return produk
         raise ProductNotAvailableError(f'Produk {nama} tidak ditemukan dalam inventaris.')
+    
+    def lihat_gudang(self):
+        if not self.products:
+            print('Gudang Kosong.')
+        else:
+            print('Daftar produk dalam gudang.')
+            for produk in self.products:
+                print(f'Nama: {produk.nama:10} | Harga: {produk.harga:10,.0f} | Stok: {produk.stok:10,.0f}')
+        
 
 if __name__ == "__main__":
-    print()
-    print('==================================')
-    print("=== Program Sistem Inventaris ===")
-    print('==================================')
+    while True:
+        print()
+        print('==================================')
+        print("=== Program Sistem Inventaris ===")
+        print('==================================')
+        print('1. Tambah Produk')
+        print('2. Kurangi Stok')
+        print('3. Cari Produk')
+        print('4. Lihat Gudang')
+        print('5. Keluar')
+        
+        user = input('Masukkan pilihan (1-5): ')
+        
+        if user == '1':
+            nama = input('Masukkan nama produk: ')
+            harga = float(input('Masukkan harga produk: '))
+            stok = int(input('Masukkan stok produk: '))
+            inventory = Inventory()
+            inventory.tambah_produk(nama, harga, stok)
+            
+        elif user == '2':
+            nama = input('Masukkan Nama produk: ').lower()
+            jumlah = int(input('Masukkan jumlah: '))
+            inventory = Inventory()
+            try:
+                inventory.kurangi_stok(nama, jumlah)
+            except ProductNotAvailableError as e:
+                print(e)
+            
+        elif user == '3':
+            nama = input('Masukkan nama produk: ').lower()
+            inventory = Inventory()
+            try:
+                produk = inventory.cari_produk(nama)
+                print(f'Produk ditemukan: {produk.nama}, Harga: {produk.harga}, Stok: {produk.stok}')
+            except ProductNotAvailableError as e:   
+                print(e)
+            
+        elif user == '4':
+            inventory = Inventory()
+            inventory.lihat_gudang()
+        
+        elif user == '5':
+            print('Terima kasih telah menggunakan program ini.')
+            break
+            
+        else:
+            print('Pilihan tidak valid. Silakan pilih antara 1-4.')
     
-    print()
-    print()
-    
-    inventaris = Inventory()
-    inventaris.tambah_produk("Laptop", 15000000, 10)
-    inventaris.tambah_produk("Mouse", 150000, 50)
-    
-    try:
-        inventaris.kurangi_stok("Laptop", 5)
-        inventaris.kurangi_stok("Mouse", 60)  # Akan memicu ProductNotAvailableError
-    except ProductNotAvailableError as e:
-        print(f"[ERROR]: {e}")
